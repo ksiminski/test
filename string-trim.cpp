37fa27178438c82a4a1b06d9d349e331d0530bca
ksiminski <ks@abc.pl>
2015-09-27 20:43:05 +0200

    pierwsze zatwierdzenie

---
+++ string-trim.cpp
@@ -0,0 +1,18 @@
+
+
+std::string trim (std::string s)
+{
+    std::string white (" \t\f\v\n\r");
+    std::string::size_type pos = s.find_last_not_of (white);
+    if (pos != std::string::npos)
+    {
+       s.erase(pos + 1);
+       pos = s.find_first_not_of(white);
+       if (pos != std::string::npos)
+          s.erase(0, pos);
+    }
+    else
+       s.erase(s.begin(), s.end());
+
+    return s;
+}
